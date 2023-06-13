3869476defef991f773297c8e6a8df596cbb3593
Jewel Osafile <osafilejewel@gmail.com>
2023-06-13 16:31:49 +0100

    user routes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
 import express from 'express'
+import userRoutes from './routes/userRoutes'
 
 const app = express()
 app.use(express.json())
+
+
+app.use('/user', userRoutes)
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+
+
+
+
+app.listen(5000, () => {
+    console.log(`Server running on port 5000`)
+})
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,6 +3,39 @@ import { Router } from "express";
 const router = Router();
 
 
+//user end Points
+//create User
+router.post('/', (req, res) => {
+    res.status(501).json({error: 'Not implemented'})
+})
+
+//list Users
+
+router.get('/', (req, res) => {
+    res.status(501).json({error: 'Not implemented'})
+})
+
+//get One user
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    res.status(501).json({error: `Not implemented ${id}`})
+})
+
+
+//Update User
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    res.status(501).json({error: `Not implemented ${id}`})
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    res.status(501).json({error: `Not implemented ${id}`})
+})
+
+
 
 
 export default router;
