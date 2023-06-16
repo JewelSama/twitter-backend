e632f970eee0c4fc27e4703d6150949609d53e6e
Jewel Osafile <osafilejewel@gmail.com>
2023-06-16 11:54:29 +0100

    auth route

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express"
+
+const router = Router();
+
+
+// Create a user if it doesn't exist,
+// Generate email token and send it to email
+router.post('/login', async(req, res) => {
+    
+})
+
+
+//Validate the email token and exchange 
+
+export default router;
