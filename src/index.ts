e7a8d13edf64784c3c1d4d6adb76e666ec56c1ed
Jewel Osafile <osafilejewel@gmail.com>
2023-06-16 21:24:21 +0100

    dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@ import express from 'express'
 import userRoutes from './routes/userRoutes'
 import tweetRoutes from './routes/tweetRoutes'
 import authRoutes from './routes/authRoutes'
+import { authenticateToken } from './middlewares/authmiddleware'
+import dotenv from "dotenv"
 
+dotenv.config()
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
-app.use('/user', userRoutes)
-app.use('/tweet', tweetRoutes)
+app.use('/user', authenticateToken, userRoutes)
+app.use('/tweet', authenticateToken, tweetRoutes)
 app.use('/auth', authRoutes)
 
 app.get('/', (req, res) => {
